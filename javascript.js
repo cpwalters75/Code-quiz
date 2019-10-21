4985202850b06765c7629cfde90dab6c21605f8b
Chris Walters <cpwalters75@gmail.com>
2019-10-20 21:37:40 -0400

    commented code

--- javascript.js
+++ javascript.js
@@ -1,14 +1,22 @@
-//Variables start button and  timeer 
+// Watched a video from web dev simplified 
+//Variables start button, next button, questions and buttons
 var startButton= document.getElementById('start-btn')
 var nextButton= document.getElementById('next-btn')
 var questionContainerElement = document.getElementById("question-container")
 var questionElement = document.getElementById("question")
 var answerButtonsElement = document.getElementById("answer-buttons")
 
+//Variable to randomize the questions
 var shuffledQuestions, currentQuestionIndex
 
+//Start and Next click events 
 startButton.addEventListener('click', startGame)
+nextButton.addEventListener("click", () =>{
+    currentQuestionIndex++
+    setNextQuestion()
 
+})
+//Function to start game
 function startGame(){
     console.log("started")
     startButton.classList.add("hide")
@@ -18,6 +26,7 @@ function startGame(){
     setNextQuestion()
 }
 
+//Function so currect questions clears for next question
 function setNextQuestion(){
     clearState()
 
@@ -25,7 +34,7 @@ function setNextQuestion(){
 
 
 }
-
+//Question appear
 function showQuestion(question){
     
     questionElement.innerText = question.question
@@ -33,6 +42,8 @@ function showQuestion(question){
         var button = document.createElement("button")
         button.innerText = answer.text
         button.classList.add('btn')
+
+    // if statment4 to check to see if the answer selected is the correct data attribute
         if (answer.correct) {
             button.dataset.correct = answer.corret
         }
@@ -44,6 +55,7 @@ function showQuestion(question){
    
 
 }
+// clear answer buttons for next set
 function clearState() {
     nextButton.classList.add("hide")
     while (answerButtonsElement.firstChild){
@@ -52,17 +64,28 @@ function clearState() {
         )
     }
 }
-
+//Event target used to select answers 
 function selectAnswer(e){
     var selectedButton = e.target
     var correct = selectedButton.dataset.correct
     setStatusClass(document.body, correct)
-    Array.from(answerButtonsElement.children).forEach(button =>{
+    Array.from(answerButtonsElement.children).forEach(button => {
         setStatusClass(button, button.dataset.correct)
 
     })
 
+    // if else to run through the length of the questions 
+    if (shuffledQuestions.length > currentQuestionsIndex + 1) {
+        nextButton.classList.remove("hide")
+    } else {
+        startButton.innerText = "restart"
+        startButton.classList.remove("hide")
+
+    }
+    nextButton.classList.remove("hide")
 }
+
+
 function setStatusClass(element, correct){
     clearStatusClass(element)
     if (correct){
@@ -75,6 +98,8 @@ function clearStatusClass(element){
     element.classList.remove("correct")
     element.classList.remove("wrong")
 }
+
+//question variables 
 var questions = [
     {
         question: "Inside which HTML element do we put the JavaScript?",
@@ -86,7 +111,27 @@ var questions = [
         ]
 
 
-    }
+    },
+    {
+        question: "How do you write Hello World in an alert box?",
+        answers: [
+            {text: "alert(Hello World)", correct: true},
+            {text: "alertBox(Hello World)", correct: false},
+            {text: "msgBox(Hello World)", correct: false}, 
+            {text: "msg(Hello World)", correct: false}
+         ]
+     },
+     {
+         question: "How to write an IF statement for executing some code if i is NOT equal to 5?",
+         answers:[
+            {text: "if i =! 5 then", correct: false},
+            {text: "if (i <>5)", correct: false},
+            {text: "if(i!=5", correct: true}
+
+
+
+         ]
+     }
 
 
 
